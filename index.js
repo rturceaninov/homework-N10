577a584349b993b03bbea1e53173356ff08e215f
rturceaninov <43860405+rturceaninov@users.noreply.github.com>
2018-11-08 17:22:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var myCurentAge = 34;
+for (var i = 3; i<=myCurentAge; i=i+3){
+    console.log("AGEmultiples_of_3",i);}
+    for (var i = myCurentAge; i<=myCurentAge; i=i+3); {
+        alert("myCurentAge 34");
+    }
